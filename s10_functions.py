2c1b9b99ae964e79ed7284ffadea34d2beb9079a
Dyana Wu <dyanawu@gmail.com>
2020-05-23 16:24:20 +0800

    Update function ex

--- s10_functions.py
+++ s10_functions.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-from functools import reduce
-
 def validate_input(s: str) -> bool:
     try:
         int(s)
@@ -21,20 +19,13 @@ def contains_only_valid(digits: list) -> bool:
     return False
 
 
-def sums_even(digits: list) -> bool:
-    sum_ = sum(digits)
-    if sum_ %2 == 0:
-        return True
-    return False
-
-
 def main():
     entered = ""
     while not validate_input(entered):
         entered = input("Enter a whole number: ")
         if validate_input(entered):
             digits = get_digits(entered)
-            print(contains_only_valid(digits) and sums_even(digits))
+            print(contains_only_valid(digits) and len(digits) % 2 == 0)
             break
         print(f"{entered} is not a whole number.")
 
