4ac73d0cdcc87f3c57389aec15029ddcdd884963
Dyana Wu <dyanawu@gmail.com>
2020-05-23 16:37:56 +0800

    Update functions challenge

--- s10_functions.py
+++ s10_functions.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-def validate_input(s: str) -> bool:
-    try:
-        int(s)
-    except ValueError:
-        return False
-    return True
-
-
-def get_digits(s: str) -> list:
-    return [int(i) for i in s]
-
-
 def contains_only_valid(digits: list) -> bool:
     valid_digits = {3, 5, 7}
     digits = set(digits)
@@ -20,15 +8,9 @@ def contains_only_valid(digits: list) -> bool:
 
 
 def main():
-    entered = ""
-    while not validate_input(entered):
-        entered = input("Enter a whole number: ")
-        if validate_input(entered):
-            digits = get_digits(entered)
-            print(contains_only_valid(digits) and len(digits) % 2 == 0)
-            break
-        print(f"{entered} is not a whole number.")
-
+    entered = input("Enter a whole number: ")
+    digits = [int(i) for i in entered]
+    print(contains_only_valid(digits) and len(digits) % 2 == 0)
 
 if __name__ == '__main__':
     main()
